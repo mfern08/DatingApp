4fde6d358d2fd865a597c5cbd3f6ddd4fb4236d8
Maria Fernandez <airamreltub@gmail.com>
2021-04-26 22:03:05 -0700

    added testing

---
+++ app/src/androidTest/java/com/example/datingapp/MainActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.datingapp;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+//@RunWith(AndriodJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void openSecondActivity(){
+        onView(withId(R.id.button)).perform(click());
+    }
+}
+
